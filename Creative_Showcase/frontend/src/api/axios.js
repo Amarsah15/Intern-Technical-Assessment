a97bc80e5b13e0f0dc293a526c486d431a4688ff
Amarsah15 <gsah150803@gmail.com>
2025-12-27 21:48:25 +0530

    fix: update API base URL to include version and set default headers

--- Creative_Showcase/frontend/src/api/axios.js
+++ Creative_Showcase/frontend/src/api/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://creative-showcase-amarnath-kumar.onrender.com",
+  baseURL: "https://creative-showcase-amarnath-kumar.onrender.com/api/v1",
   withCredentials: true, // IMPORTANT for JWT cookies
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default api;
